22fd0506cfcb95ff5e6ae2c946d7c2c25ea3a044
Wayne Graham <wayne.graham@virginia.edu>
2013-04-26 15:00:44 -0400

    Migrate User data

--- migrate.rb
+++ migrate.rb
@@ -8,8 +8,28 @@ development_database = 'prism_develop'
 
 conn = PG.connect(dbname: production_database)
 
+sql = ""
+
+users_sql = ""
+
 conn.exec("SELECT * FROM Users") do |result|
   result.each do |row|
-    pp row.values_at('id')
+    users_sql += "INSERT INTO users(id, email, encrypted_password, sign_in_count, current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, created_at, updated_at) 
+    VALUES( %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');\n" %
+      row.values_at(
+        'id',
+        'email',
+        'encrypted_password',
+        'sign_in_count',
+        'current_sign_in_at',
+        'last_sign_in_at',
+        'current_sign_in_ip',
+        'last_sign_in_ip',
+        'created_at',
+        'updated_at'
+    )
   end
 end
+
+
+
