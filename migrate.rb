9a32a73561ca90308b056ba43a82f5d5022c786e
Wayne Graham <wayne.graham@virginia.edu>
2013-04-26 16:50:40 -0400

    Updates to write the SQL file

--- migrate.rb
+++ migrate.rb
@@ -6,16 +6,22 @@ require 'pp'
 production_database = 'prism'
 development_database = 'prism_develop'
 
-conn = PG.connect(dbname: production_database)
+file_name = 'migration.sql'
 
-sql = ""
+doc9 = "24296ef2-aea5-11e2-80bf-c82a14fffe99"
+doc10 = "3b56ee42-aea5-11e2-80bf-c82a14fffe99"
+doc11 = "4213c156-aea5-11e2-80bf-c82a14fffe99"
+doc12 = "480e952c-aea5-11e2-80bf-c82a14fffe99"
+
+
+conn = PG.connect(dbname: production_database)
 
 users_sql = ""
 
 conn.exec("SELECT * FROM Users") do |result|
   result.each do |row|
     users_sql += "INSERT INTO users(id, email, encrypted_password, sign_in_count, current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, created_at, updated_at) 
-    VALUES( %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');\n" %
+    VALUES( %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');\n\n" %
       row.values_at(
         'id',
         'email',
@@ -31,5 +37,11 @@ conn.exec("SELECT * FROM Users") do |result|
   end
 end
 
+conn.exec("SELECT * FROM FACETS") do |result|
 
+end
+
+
+
+File.open(file_name, 'w') { |file| file.write(users_sql) }
 
