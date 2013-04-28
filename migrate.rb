c244ad596b76ccb4da0ce577c3556424a44f105d
Wayne Graham <wayne.graham@virginia.edu>
2013-04-28 11:09:31 -0400

    migrate the facets table directly

--- migrate.rb
+++ migrate.rb
@@ -4,7 +4,7 @@ require 'pg'
 require 'pp'
 
 production_database = 'prism'
-development_database = 'prism_develop'
+development_database = 'prism_development'
 
 file_name = 'migration.sql'
 
@@ -13,35 +13,68 @@ doc10 = "3b56ee42-aea5-11e2-80bf-c82a14fffe99"
 doc11 = "4213c156-aea5-11e2-80bf-c82a14fffe99"
 doc12 = "480e952c-aea5-11e2-80bf-c82a14fffe99"
 
+doc_map = {
+  "9" => "24296ef2-aea5-11e2-80bf-c82a14fffe99",
+  "10" => "3b56ee42-aea5-11e2-80bf-c82a14fffe99",
+  "11" => "4213c156-aea5-11e2-80bf-c82a14fffe99",
+  "12" => "480e952c-aea5-11e2-80bf-c82a14fffe99"
+}
+
 
 conn = PG.connect(dbname: production_database)
+db = PG.connect(dbname: development_database)
+db.set_error_verbosity(PG::PQERRORS_VERBOSE)
+
 
 users_sql = ""
+facets_sql = ""
 
 conn.exec("SELECT * FROM Users") do |result|
   result.each do |row|
     users_sql += "INSERT INTO users(id, email, encrypted_password, sign_in_count, current_sign_in_at, last_sign_in_at, current_sign_in_ip, last_sign_in_ip, created_at, updated_at) 
     VALUES( %s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');\n\n" %
-      row.values_at(
-        'id',
-        'email',
-        'encrypted_password',
-        'sign_in_count',
-        'current_sign_in_at',
-        'last_sign_in_at',
-        'current_sign_in_ip',
-        'last_sign_in_ip',
-        'created_at',
-        'updated_at'
+    row.values_at(
+      'id',
+      'email',
+      'encrypted_password',
+      'sign_in_count',
+      'current_sign_in_at',
+      'last_sign_in_at',
+      'current_sign_in_ip',
+      'last_sign_in_ip',
+      'created_at',
+      'updated_at'
     )
   end
 end
 
-conn.exec("SELECT * FROM FACETS") do |result|
+def migrate_facets
 
+  facet_counter = 1
+  conn.exec("SELECT * FROM facets") do |results|
+    results.each do |row|
+
+      id = row.values_at('id').first
+      color = row.values_at('color').first
+      description = row.values_at('category').first
+      created_at = row.values_at('created_at').first
+      updated_at = row.values_at('updated_at').first
+      prism_id = doc_map[row.values_at('prism_id').first]
+
+      db.prepare("facets#{facet_counter}", "INSERT INTO facets(id, color, description, created_at, updated_at, prism_id) VALUES($1, $2, $3, $4, $5, $6)")
+
+      db.exec_prepared("facets#{facet_counter}", [id, color, description, created_at, updated_at, prism_id])
+      facet_counter += 1
+    end
+
+
+  end
 end
 
 
 
-File.open(file_name, 'w') { |file| file.write(users_sql) }
+
+#migrate_facets
+
+#File.open(file_name, 'w') { |file| file.write(users_sql) }
 
